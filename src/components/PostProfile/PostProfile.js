4810ee685839fb9a5829d538ed4cd150b74ede3c
Isabela Ortiz <i.ortiz@uniandes.edu.co>
2022-12-04 22:10:35 -0500

    avances PWA user y pet

--- src/components/PostProfile/PostProfile.js
+++ src/components/PostProfile/PostProfile.js
@@ -5,65 +5,88 @@ import PostPet from "../PostPet/PostPet";
 import "./PostProfile";
 import { FormattedMessage } from 'react-intl';
 import Login from '../SignIn/login/login';
-import CreateEvent from '../CreateEvent/create-event';
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function PostProfile() {
-    const [profile, setProfiles] = useState([]);
+   
 
-    useEffect(() => {
-        const URL =
-            "http://localhost:3000/api/v1/users/e9aaea36-9a0d-44aa-a9ba-ac5b01e6810c";
+
+    const [profile, setProfiles] = useState([]);
+    useEffect(()=>{
+        if(!navigator.onLine){
+            if(localStorage.getItem("user-complete") != null) {
+                setProfiles(localStorage.getItem("user-complete"));
+            }
+        } else {
+            const URL =
+            'http://localhost:3000/api/v1/users/'+ localStorage.getItem('user');
         fetch(URL)
             .then((data) => data.json())
             .then((data) => {
                 setProfiles(data);
-            });
+                localStorage.setItem("user-complete", data);
+                console.log("data " + data)
+            })
+        }
     }, []);
 
+
+
+
     const [pets, setPets] = useState([]);
-    useEffect(() => {
-        const URL =
-            "http://localhost:3000/api/v1/users/e9aaea36-9a0d-44aa-a9ba-ac5b01e6810c";
-        fetch(URL)
-            .then((data) => data.json())
-            .then((data) => {
-                setPets(data.pets);
-            });
+
+    useEffect(()=>{
+        if(!navigator.onLine){
+            if(localStorage.getItem("pets") != null) {
+                setPets(localStorage.getItem("pets"));
+            }
+        } else {
+            const URL =
+            'http://localhost:3000/api/v1/users/' + localStorage.getItem('user');
+            fetch(URL)
+                .then((data) => data.json())
+                .then((data) => {
+                    setPets(data.pets);
+                    localStorage.setItem("pets", data.pets);
+
+                })
+           
+        }
     }, []);
 
     return (
         <Element.Container className='main-container' >
+            <br></br>
+            <br></br>
+
             < div class="container, PostProfile">
-                <div class="row">
-                    <div class="col-sm">
+                <Row>
+                  <Col>
                     <h1 scope="col"> <FormattedMessage id="Profile" /></h1>
-                    </div>
-                    <div class="col-8">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-2">
-                    </div>
-                    <div class="col-2">
+                </Col>              
+                </Row>
+                <Row>              
+                   <Col>
                         <img src={perfil} alt="icono perfil" height="150" width="150" />
-                    </div>
-                    <div class="col-8">
+                        </Col>
+                        <Col>
                         <h3>{profile.name}</h3>
                         <h4>{profile.login}</h4>
                         <h4>{profile.city}</h4>
-                    </div>
-                </div>
+                        </Col>
+                    </Row>
             </div>
             <div>
-            <h1 scope="col"> <FormattedMessage id="MyPets" /></h1>
-
+            <br></br>
+            <br></br>
+            <h1 scope="row"> <FormattedMessage id="MyPets" /></h1>
                 {pets.map(elem => {
                     return (
                         <PostPet name={elem.name} species={elem.species} image={elem.image} ></PostPet>)
                 })}
             </div>
-            <CreateEvent></CreateEvent>
+
         </Element.Container>
 
     );
